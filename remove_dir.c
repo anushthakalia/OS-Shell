acd92797935661d68f95e25853b3cf9c84f1daab
Anushtha Kalia <anushthakalia@gmail.com>
2016-10-14 00:18:01 +0530

    remove a dir without any subfolders
    working on the code for subfolders too

---
+++ remove_dir.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main()
+{
+    // These are data types defined in the "dirent" header
+    char str[100];
+    printf("Enter the directory to delete");
+    scanf("%s",str);
+    DIR *theFolder = opendir(str);
+    struct dirent *next_file;
+    char filepath[256];
+
+
+
+    while ( (next_file = readdir(theFolder)) != NULL )
+    {
+        // build the path for each file in the folder
+        sprintf(filepath, "%s/%s", str, next_file->d_name);
+        remove(filepath);
+        
+       
+    }
+    rmdir(str);
+    printf(" Folder Deleted");
+    closedir(theFolder);
+    return 0;
+}
